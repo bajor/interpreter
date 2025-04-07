5051bb884cb468a635b5e8d9d30cf0c71f38446c
Maciek Bajor <maciekbajor@gmail.com>
2025-04-07 12:59:36 +0200

    Add parserc lib

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
+
 import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec (Parser, oneOf)
 -- import Lib
 
 main :: IO ()
@@ -8,6 +10,11 @@ main :: IO ()
 main = do
     args <- getArgs
     let name = args !! 0
+    putStrLn ("hello " ++ name)
+
+    let symbol :: Parser Char
+        symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
     putStrLn ("hello " ++ name)
 
 -- How to run it: `stack run Andrzej` will say `hello Andrzej`
