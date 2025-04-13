53b9ce86cb96ac63c959b6cf7e33f65ff1cd6c2d
Maciek Bajor <maciekbajor@gmail.com>
2025-04-13 12:09:32 +0200

    Add LispString parser and Lisp Values as ADT

--- src/ReadExpr.hs
+++ src/ReadExpr.hs
@@ -1,13 +1,25 @@
 module ReadExpr (readExpr) where
 
-import Text.ParserCombinators.Parsec (Parser, oneOf, parse, skipMany1, space)
+import Text.ParserCombinators.Parsec (Parser, oneOf, parse, skipMany1, space, noneOf, char, many)
+import Types
 
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+
 spaces :: Parser ()
 spaces = skipMany1 space
+
+
+parseLispString :: Parser LispVal
+parseLispString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ Types.LispString x
+  -- A string is a double quote mark, followed by any number of non-quote characters, followed by a closing quote mark
+
 
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
---
+++ src/Types.hs
@@ -0,0 +1,14 @@
+module Types where
+
+data LispVal = 
+   LispAtom String
+  | LispList [LispVal]
+  | LispDottedList [LispVal] LispVal
+  | LispNumber Integer
+  | LispString String
+  | LispBool Bool
+
+-- ADT - Algebraic Data Type - defines set possible values that are possible in Lisp
+
+-- LispDottedList 	- representing the Scheme form (a b . c); also called an improper list. This stores a list of all elements but the last, and then stores the last element as another field
+
